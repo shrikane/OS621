166cf38b77b92ea7ee764b3f261658eb23c48c53
shrikane <skane3@umbc.edu>
2014-04-26 12:18:04 -0700

    Bug Fixed for loop

--- BasicStuff/com.erl
+++ BasicStuff/com.erl
@@ -8,7 +8,7 @@
 %% ====================================================================
 %% API functions
 %% ====================================================================
--export([ start/0, initProc/1, process/1]).
+-export([ start/0, initProc/2, process/2]).
 
 
 
@@ -18,32 +18,43 @@
 
 
 start() ->
-initProc(5).
+initProc(5,5),
+     Msg =[1,88,99],
+     whereis('P_5') ! Msg.
 
 
-process(0) ->
+
+process(0,5) ->
     true;
 
-process(Limit) ->
+process(Limit,N) ->
 	       	       
 	      % io:format("in procc method ~n "),
-	       put("Neighbour",[Limit+1,Limit-1]),
-	       put("frag", [random:uniform(20) ,random:uniform(20)]),
+	       put("Neighbour",[(Limit+1) rem N ,(Limit-1) rem N]),
+	       put("frag", [random:uniform(20) ,random:uniform(20),Limit]),
 	       io:format("Neighbour are: ~p  ~n ",[get("Neighbour")]),
 	       io:format("NKeys: ~p  ~n ",[get("frag")]),
 	       Newcount = Limit -1,
 	      % io:format(" NewCount: ~p ~n", [Newcount]),
-	       initProc(Newcount).
-	 %end.      
+	       initProc(Newcount,N),
+	 %end. 
+		
+	 receive 
+	Msg ->
+	  io:format("Keys in rec: ~p  ~n ",[get("frag")])
+    end.
 
 
-initProc(0) -> true;
+initProc(-1,N) -> 
+%io:format("NP_id: ~p  ~n ",[]),
+true;
 
-initProc(Limit) ->  
+initProc(Limit,N) ->  
     % io:format("Limit is ~p ~n ", [Limit]),
-     Processname = list_to_atom(string:concat( "p_" ,integer_to_list(Limit))),
-     register(Processname ,spawn_link(com , process , [Limit])),
-     io:format("Forked ~p ~n",[Processname]).
+     Processname = list_to_atom(string:concat( "P_" ,integer_to_list(Limit))),
+     register(Processname ,spawn_link(com , process , [Limit,N])),
+
+io:format("Forked ~p ~n",[Processname]).
      
      
      
