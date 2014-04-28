ea59c39bd51771c27fe8ea6851ccb0cb9dcb2edc
shrikane <skane3@umbc.edu>
2014-04-27 23:50:52 -0700

    Yin's changes

--- BasicStuff/com.erl
+++ BasicStuff/com.erl
@@ -1,5 +1,3 @@
-%% @author Shrinivas
-%% @doc @todo Add description to com.
 
 
 -module(com).
@@ -8,7 +6,7 @@
 %% ====================================================================
 %% API functions
 %% ====================================================================
--export([ start/0, initProc/2, process/2]).
+-export([ start/0, initProc/2, process/2, segfile/2]).
 
 
 
@@ -18,13 +16,59 @@
 
 
 start() ->
+segfile("./data.dat",2),
 initProc(5,5),
      Msg =[1,88,99],
      whereis('P_5') ! Msg.
+	
+
+%%  author: Yin Huang
+%%  Input: 1. filename to be segmented for example: ""./data/data.dat"
+%%         2. Size of the chunk (all equal) so the number of fragments = size of data / size of the chunk.
+%%  Output: same size chunks of the original file written to ./seg/F_index
+%%  Note:   output directory is hard coded as under ./seg so you need to mkdir seg under current directory 
 
 
 
-process(0,5) ->
+for(Max, Max, F) ->
+    [F(Max)];
+for(I, Max,F) ->
+    [F(I)|for(I+1,Max, F)].
+
+segfile(FileName,S) ->
+    {ok, Binary} = file:read_file(FileName),
+    Lines = string:tokens(erlang:binary_to_list(Binary), "\n"),
+    io:format("~p~n",[Lines]),
+    MyLists = [ lists:sublist(Lines, X, S) || X <- lists:seq(1,length(Lines),S) ],
+    io:format("~p~n", [MyLists] ),
+    io:format("~p~n", [length(MyLists)]),
+    %%io:format("~p~n",[lists:nth(2,MyLists)]),
+    	
+      for(1,length(MyLists), fun(Index) ->
+     				    filewrite(string:concat("./seg/F_", integer_to_list(Index)),lists:nth(Index,MyLists)),
+	                           io:format("~p has been written to ~p ~n", [FileName,string:concat("./seg/F_", integer_to_list(Index)) ])
+					end
+	).
+
+
+filewrite(File, L) ->
+   case file:open(File, [write]) of
+      {ok, Device} ->
+         lists:foreach(fun(Line) -> writeline(Device, Line) end, L),
+         file:close(Device)
+   end.
+writeline(Device, Line) -> writeline(Device, Line, os:type()).
+
+writeline(Device, Line, {win32,_}) -> io:fwrite(Device, "~s\r\n", [Line]);
+writeline(Device, Line, _) -> io:fwrite(Device, "~s\n", [Line]).
+
+
+%%  author: Shrinivas Kane
+%%  Input: 1. Process ID number
+%%         2. Number of process to be spawn
+
+
+process(-1,5) ->
     true;
 
 process(Limit,N) ->
