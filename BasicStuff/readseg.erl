0fc25e66d7a64f4af3421305ef7792b51e77011a
yinlosky <yinlosky@yahoo.com>
2014-04-28 17:52:58 -0400

    Update readseg.erl

--- BasicStuff/readseg.erl
+++ BasicStuff/readseg.erl
@@ -11,6 +11,8 @@ generatelist(FilePath, Destination)->
 	io:format("reading from ~p~n",[FilePath]),
     {ok, Filenames} = file:list_dir(FilePath),
     io:format("~p contains following files ~p~n", [FilePath, Filenames]),
+    L = length(Filenames),
+    io:format("Length is ~p~n",[L]),
       for(1,length(Filenames), fun(Index) ->
 				       io:format("reading from ~p~n",[string:concat(FilePath,lists:nth(Index,Filenames))]),
 		        {ok, Binary} = file:read_file(string:concat(FilePath,lists:nth(Index,Filenames))),
@@ -18,8 +20,12 @@ generatelist(FilePath, Destination)->
 				  %     io:format(" ~p", [string:tokens(erlang:binary_to_list(Binary), "\n")])
 		        Lines = lists:append([string:substr(lists:nth(Index,Filenames),3,1)],string:tokens(erlang:binary_to_list(Binary), "\n") ),
 		       io:format("~p~n",[Lines]),
-					   file:write_file(Destination,io_lib:fwrite("~p,",[Lines]),[append])
+				       case Index of
+					   L
+					        -> file:write_file(Destination,io_lib:fwrite("~p",[Lines]),[append]);
+					   _Other ->  file:write_file(Destination,io_lib:fwrite("~p,",[Lines]),[append])
 					   end
+					  end
 	  ),
 	  io:format("Files in ~p have been read and stored into ~p", [FilePath, Destination]).
     
